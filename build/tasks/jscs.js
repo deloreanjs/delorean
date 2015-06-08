4864e0712d4ad0f6f286487516f5ebe8b67ae09e
craigdallimore <decoy9697@gmail.com>
2015-06-08 10:50:30 +0100

    Allow karma.conf to have snake_case identifiers
    The VirtualBox launcher config uses a different naming convention, so
    for karma.conf the `requireCamelCaseOrUpperCaseIdentifiers` rule is
    relaxed.

--- build/tasks/jscs.js
+++ build/tasks/jscs.js
@@ -2,13 +2,26 @@
 
 module.exports = function(grunt) {
   grunt.config('jscs', {
-    src: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js', '!test/vendor/**/*.js'],
-    options: {
-      config: '.jscsrc',
-      validateIndentation: 2,
-      disallowDanglingUnderscores: null,
-      disallowMultipleVarDecl: null,
-      requireMultipleVarDecl: null
+    main: {
+      src: ['Gruntfile.js', 'src/**/*.js', 'test/spec/**/*.js'],
+      options: {
+        config: '.jscsrc',
+        validateIndentation: 2,
+        disallowDanglingUnderscores: null,
+        disallowMultipleVarDecl: null,
+        requireMultipleVarDecl: null
+      }
+    },
+    special: {
+      src: ['test/karma.conf.js'],
+      options: {
+        config: '.jscsrc',
+        validateIndentation: 2,
+        requireCamelCaseOrUpperCaseIdentifiers: null,
+        disallowDanglingUnderscores: null,
+        disallowMultipleVarDecl: null,
+        requireMultipleVarDecl: null
+      }
     }
   });
 
